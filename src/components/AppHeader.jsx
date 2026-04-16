355831583ff68e11e01a87175b1b09f18d054042
AndreaDistefanoDev7 <andre.distefano99@gmail.com>
2026-04-16 15:12:59 +0200

    fix AppHeader

--- src/components/AppHeader.jsx
+++ src/components/AppHeader.jsx
@@ -5,24 +5,21 @@ export default function AppHeader() {
 
     return (
         <header>
-            <nav className="navbar navbar-expand bg-body-tertiary">
+            <nav className="navbar navbar-expand bg-body fw-semibold">
                 <div className="container">
                     <a className="navbar-brand" href="#"><img src={appLogo} alt="" /></a>
+                    <div className="d-flex gap-3 text-uppercase">
+                        <a className="nav-link" href="#">characters</a>
+                        <a className="nav-link" href="#">comics</a>
+                        <a className="nav-link" href="#">movies</a>
+                        <a className="nav-link" href="#">tv</a>
+                        <a className="nav-link" href="#">games</a>
+                        <a className="nav-link" href="#">collectibles</a>
+                        <a className="nav-link" href="#">videos</a>
+                        <a className="nav-link" href="#">fans</a>
+                        <a className="nav-link" href="#">news</a>
+                        <a className="nav-link" href="#">shop</a>
 
-                    <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-                        <div className="navbar-nav text-uppercase">
-                            <a className="nav-link" href="#">characters</a>
-                            <a className="nav-link" href="#">comics</a>
-                            <a className="nav-link" href="#">movies</a>
-                            <a className="nav-link" href="#">tv</a>
-                            <a className="nav-link" href="#">games</a>
-                            <a className="nav-link" href="#">collectibles</a>
-                            <a className="nav-link" href="#">videos</a>
-                            <a className="nav-link" href="#">fans</a>
-                            <a className="nav-link" href="#">news</a>
-                            <a className="nav-link" href="#">shop</a>
-
-                        </div>
                     </div>
                 </div>
             </nav>
