be661fda29fbc7f2d0d84042376c7a502d2da657
AndreaDistefanoDev7 <andre.distefano99@gmail.com>
2026-04-16 16:08:48 +0200

    create component AppMain

--- src/components/AppMain.jsx
+++ src/components/AppMain.jsx
@@ -1,7 +1,47 @@
+import digitalComicsLogo from '../assets/img/buy-comics-digital-comics.png'
+import merchandiseComicsLogo from '../assets/img/buy-comics-merchandise.png'
+import subscriptionsComicsLogo from '../assets/img/buy-comics-subscriptions.png'
+import locatorComicsLogo from '../assets/img/buy-comics-shop-locator.png'
+import powerVisaComicsLogo from '../assets/img/buy-dc-power-visa.svg'
+
+
+
+
+
 export default function AppMain() {
 
 
     return (
-        <main>oooo</main>
+        <main>
+            <div className="bg-black text-white">
+                <section id='section_black'>
+                    <p>__Content here--</p>
+                </section>
+            </div>
+            <div className="bg-primary text-white text-uppercase">
+                <div className="row">
+                    <div className="col">
+                        <img src={digitalComicsLogo} alt="" />
+                        <span>digital comics</span>
+                    </div>
+                    <div className="col">
+                        <img src={merchandiseComicsLogo} alt="" />
+                        <span>dc merchandise</span>
+                    </div>
+                    <div className="col">
+                        <img src={subscriptionsComicsLogo} alt="" />
+                        <span>subscriptions</span>
+                    </div>
+                    <div className="col">
+                        <img src={subscriptionsComicsLogo} alt="" />
+                        <span>comic shop locator</span>
+                    </div>
+                    <div className="col">
+                        <img src={powerVisaComicsLogo} alt="" />
+                        <span>dc power visa</span>
+                    </div>
+                </div>
+            </div>
+        </main>
     )
 }
