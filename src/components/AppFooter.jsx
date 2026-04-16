ebfb8e9f1dbbefb9c581e76dccc0db9d1a382fec
AndreaDistefanoDev7 <andre.distefano99@gmail.com>
2026-04-16 16:56:42 +0200

    create component AppFooter

--- src/components/AppFooter.jsx
+++ src/components/AppFooter.jsx
@@ -1,7 +1,64 @@
+import logoBg from '../assets/img/dc-logo-bg.png'
+
 export default function AppFooter() {
 
 
     return (
-        <footer>ciao</footer>
+        <footer className="text-white">
+            <div id='first-section-footer'>
+                <div className="row">
+                    <div className="col">
+                        <h3>dc comics</h3>
+                        <ul>
+                            <li>Characters</li>
+                            <li>Comics</li>
+                            <li>Movie</li>
+                            <li>TV</li>
+                            <li>Games</li>
+                            <li>Videos</li>
+                            <li>News</li>
+                        </ul>
+                    </div>
+                    <div className="col">
+                        <h3>dc</h3>
+                        <ul>
+                            <li>Terms of Use</li>
+                            <li>Privacy policy (New)</li>
+                            <li>Ad Choices</li>
+                            <li>Advertising</li>
+                            <li>Jobs</li>
+                            <li>Subscriptions</li>
+                            <li>Talent Workshops</li>
+                            <li>CPSC Certificates</li>
+                            <li>Ratings</li>
+                            <li>Shop Help</li>
+                            <li>Contact Us</li>
+                        </ul>
+                    </div>
+                    <div className="col">
+                        <h3>SITES</h3>
+                        <ul>
+                            <li>DC</li>
+                            <li>MAD Magazine</li>
+                            <li>DC Kids</li>
+                            <li>DC Universe</li>
+                            <li>DC Power Visa</li>
+                        </ul>
+                    </div>
+                    <div className="col">
+                        <h3>SHOP</h3>
+                        <ul>
+                            <li>Shop DC</li>
+                            <li>Shop DC Collectibles</li>
+                        </ul>
+                    </div>
+                    <div className="col">
+                        <img src={logoBg} alt="" />
+                    </div>
+
+                </div>
+            </div>
+            <div></div>
+        </footer>
     )
 }
